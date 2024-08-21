0060333806c91795e6fa6c77bb443957515beb55
Luther Isaboke <lusaboke@gmail.com>
2024-08-22 01:15:17 +0300

    created nav bar elemnets

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <header className="App-header">
+      <nav className="app-nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+   
+    </header>
+        <div className="App">
       <h1 className="title-head">kib4n4's Portifolio</h1>
       
-    </div>
+        </div>
+  
+    </>
   );
+  
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+
+import React from'react';
+
+function Home(){
+    return(
+        <>
+        <div className="home-page">welcome to my home page</div>
+        </>
+    )
+
+}
+
+
+export default Home;
